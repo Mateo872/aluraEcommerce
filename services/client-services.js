323ef4eddbb2efbb6c9721f320305c104df9503b
Mateo Bellini <bellinimateo872@gmail.com>
2023-03-14 10:52:31 -0300

    README

--- services/client-services.js
+++ services/client-services.js
@@ -1,8 +1,8 @@
 const listProducts = () =>
-  fetch("  http://localhost:3000/products").then((resp) => resp.json());
+  fetch("http://localhost:3000/products").then((resp) => resp.json());
 
 const createProduct = (name, price, image, category, description, id) => {
-  return fetch("  http://localhost:3000/products", {
+  return fetch("http://localhost:3000/products", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,19 +19,19 @@ const createProduct = (name, price, image, category, description, id) => {
 };
 
 const deleteProduct = (id) => {
-  return fetch(`  http://localhost:3000/products/${id}`, {
+  return fetch(`http://localhost:3000/products/${id}`, {
     method: "DELETE",
   });
 };
 
 const detailProduct = async (id) => {
-  return fetch(`  http://localhost:3000/products/${id}`).then((resp) =>
+  return fetch(`http://localhost:3000/products/${id}`).then((resp) =>
     resp.json()
   );
 };
 
 const updateProduct = (name, price, image, id, category, description) => {
-  return fetch(`  http://localhost:3000/products/${id}`, {
+  return fetch(`http://localhost:3000/products/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
